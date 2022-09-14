f2119cca5862464d281fd3b724025a8a61e746d3
ROCKERZ109 <66381573+ROCKERZ109@users.noreply.github.com>
2022-09-14 09:13:46 +0530

    first

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ import chatbot
 app = Flask(__name__)
 
 
-@app.route('/api', methods=['GET'])
-def returnascii():
+@app.route('/', methods=['GET'])
+def home():
     d = {}
     inputchr = str(request.args['query'])
     ints = chatbot.predict_class(inputchr)
     answer = chatbot.get_response(ints, chatbot.intents)
-    # answer = str(ord(inputchr))
     d['output'] = answer
     return d
 
